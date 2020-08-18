02322b84553d94a7a19c2ff0e6069e5717962fd0
Romain Torond <romain.torond@ext.leroymerlin.fr>
2020-08-18 14:50:33 +0200

    docs: prepare slides for 08/20 Institute

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -5,11 +5,11 @@ const allFiles = [
   [
     '00-school',
     [
-      '00-TITLE',
+      '00-TITLE-institute',
       // professor
       '01-speaker-wgo',
       // speakers, uncomment as needed
-      //'01-speaker-rto',
+      '01-speaker-rto',
       //'01-speaker-fsa',
       '02-introduce-yourself',
       '03-repository',

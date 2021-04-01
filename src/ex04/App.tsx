15fa457596c8578673cfead614d3722af7711992
Romain Torond <romain.torond@gmail.com>
2021-04-01 10:35:34 +0200

    exo4-bis

--- src/ex04/App.tsx
+++ src/ex04/App.tsx
@@ -4,17 +4,18 @@ import { TopAppBarActionItem } from "@rmwc/top-app-bar";
 import { Header } from "../solution/Header";
 import { PersonCard } from "../solution/PersonCard";
 
-import { Carousel } from "../ex03/Carousel";
+import { Carousel } from "./Carousel";
 
 type AppProps = {
   people: People;
 };
 
 export const App: React.FC<AppProps> = ({ people }) => {
-  const [isListPage, setListPage] = useState(true);
+  const [isListPage, setListPage] = useState(false);
 
-  const list = people.map(person => <PersonCard key={person.id} person={person} />);
-  const carousel = <Carousel people={people} />;
+  const personCards = people.map(person => <PersonCard key={person.id} person={person} />);
+
+  const carousel = <Carousel>{personCards}</Carousel>;
 
   return (
     <>
@@ -24,7 +25,7 @@ export const App: React.FC<AppProps> = ({ people }) => {
       </Header>
       <main>
 
-        {isListPage ? list : carousel }
+        {isListPage ? personCards : carousel }
 
       </main>
     </>
--- src/ex04/Carousel.tsx
+++ src/ex04/Carousel.tsx
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import { Fab } from "@rmwc/fab";
 
-import { range } from "../utils";
-
 type CarouselProps = {
   children: React.ReactElement[];
 };
 
 export const Carousel: React.FC<CarouselProps> = ({ children }) => {
-  useState()
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  const nextIndex = currentIndex === children.length-1 ? 0 : currentIndex + 1;
+  const previousIndex = currentIndex === 0 ? children.length-1 : currentIndex - 1;
+
+  const onNext = () => {
+    setCurrentIndex(nextIndex);
+  };
+  const onPrevious = () => {
+    setCurrentIndex(previousIndex);
+  };
 
   return (
     <div className="flex-row">
-      <Fab icon="skip_previous" mini />
+      <Fab icon="skip_previous" mini onClick={onPrevious}/>
 
       <div className="carousel">
 
-        {React.cloneElement(children[0], { className: "prev"})}
-        {React.cloneElement(children[1], { className: "current"})}
-        {React.cloneElement(children[2], { className: "next"})}
+        {React.cloneElement(children[previousIndex], { className: "prev"})}
+        {React.cloneElement(children[currentIndex], { className: "current"})}
+        {React.cloneElement(children[nextIndex], { className: "next"})}
 
-        Display PersonCards here. The current needs to have className="current",
-        the previous "prev" and the next "next" respectively. Use
-        React.cloneElement to add className prop to children
       </div>
-      <Fab icon="skip_next" mini />
+
+      <Fab icon="skip_next" mini onClick={onNext}/>
     </div>
   );
 };

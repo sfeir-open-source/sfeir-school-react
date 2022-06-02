2539e8580a718282660287c10097899998254c07
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-06-02 18:08:53 +0200

    Working search without much styling nor organization

--- apps/solution/src/components/people/people.tsx
+++ apps/solution/src/components/people/people.tsx
@@ -1,18 +1,47 @@
 import { Grid } from '@libs/design';
+import { Dispatch, SetStateAction, useState } from 'react';
 import Person from '../person/person';
 import usePeople from '../person/usePeople';
 
 import style from './people.module.scss';
 
+function normalize(text: string) {
+  return text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+}
+
 export function People() {
   const people = usePeople();
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const filteredPeople = people?.filter(person => {
+    const searchTermNormalized: string = normalize(searchTerm)
+    return normalize(person.lastname).includes(searchTermNormalized) || normalize(person.firstname).includes(searchTermNormalized)
+  })
 
   return (
-    <Grid className={style.container}>
-      {people?.map((person) => <Person key={person.id} person={person} />) ||
-        'Loading ...'}
-    </Grid>
+    <>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <Grid className={style.container}>
+        {filteredPeople?.map((person) => <Person key={person.id} person={person} />) ||
+          'Loading ...'}
+      </Grid>
+    </>
   );
+}
+
+
+export interface SearchBarProps {
+  searchTerm: string;
+  setSearchTerm: Dispatch<SetStateAction<string>>;
+}
+function SearchBar({ searchTerm, setSearchTerm }: SearchBarProps) {
+
+  return (
+    <>
+      <label htmlFor="searchBar">Search</label>
+      <input className={style.searchBar} type="text" name="searchBar" id="searchBar" onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} />
+    </>
+  )
 }
 
 export default People;

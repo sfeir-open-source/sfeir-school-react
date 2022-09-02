da30ba7452580ecca304c6cdb5c3413a67a2872d
Enes KOC <koc.e@sfeir.com>
2022-09-02 15:22:24 +0200

    :art: [UPDATE] UseManger less confusing, removed useless div in panel

--- apps/solution/src/components/EditPerson/EditPerson.tsx
+++ apps/solution/src/components/EditPerson/EditPerson.tsx
@@ -21,33 +21,31 @@ function EditPeople() {
   if (!person) return <div>Loading...</div>;
 
   return (
-    <div>
-      <Panel data-cy={`person-panel-${id}`} className={styles.panel}>
-        {!showForm ? (
-          <EditPersonDisplay person={person} />
-        ) : (
-          <EditPersonForm
-            person={person}
-            updatePersonAction={updatePersonAction}
-          />
-        )}
-        <div className={styles['btn-container']}>
-          <Iconized
-            icon={solid('pen-to-square')}
-            size={'2x'}
-            className={styles['edit-btn']}
-            onClick={() => setShowForm(!showForm)}
-            data-cy={`edit-btn-${id}`}
-          />
-          <Iconized
-            icon={solid('person-circle-minus')}
-            size={'2x'}
-            className={styles['delete-btn']}
-            data-cy={`delete-btn-${id}`}
-          />
-        </div>
-      </Panel>
-    </div>
+    <Panel data-cy={`person-panel-${id}`} className={styles.panel}>
+      {!showForm ? (
+        <EditPersonDisplay person={person} />
+      ) : (
+        <EditPersonForm
+          person={person}
+          updatePersonAction={updatePersonAction}
+        />
+      )}
+      <div className={styles['btn-container']}>
+        <Iconized
+          icon={solid('pen-to-square')}
+          size={'2x'}
+          className={styles['edit-btn']}
+          onClick={() => setShowForm(!showForm)}
+          data-cy={`edit-btn-${id}`}
+        />
+        <Iconized
+          icon={solid('person-circle-minus')}
+          size={'2x'}
+          className={styles['delete-btn']}
+          data-cy={`delete-btn-${id}`}
+        />
+      </div>
+    </Panel>
   );
 }
 
--- apps/solution/src/components/EditPerson/UseManagers.tsx
+++ apps/solution/src/components/EditPerson/UseManagers.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getPeople, PersonModel } from '../../api/person';
 
 export default function useManagers(currentId: string) {
   const [managers, setManagers] = useState<PersonModel[] | null>(null);
 
-  useMemo(() => {
+  useEffect(() => {
     getPeople().then((res) =>
       setManagers(res.filter((p) => p.id !== currentId))
     );

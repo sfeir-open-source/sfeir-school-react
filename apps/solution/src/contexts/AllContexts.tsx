af78cf23e44badd088c8fd1181069a593dd51368
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-09-27 18:24:10 +0200

    feat(peopleContext): adding people context but with wrong types

---
+++ apps/solution/src/contexts/AllContexts.tsx
@@ -0,0 +1,9 @@
+import { useReducer } from 'react';
+import { PersonModel } from '../api/person';
+import { PeopleContext, PeopleDispatchContext } from './PeopleContext'
+
+export default function AllContexts() {
+  return (
+    <div>Hey</div>
+  )
+}
--- apps/solution/src/contexts/PeopleContext.tsx
+++ apps/solution/src/contexts/PeopleContext.tsx
@@ -1,4 +1,43 @@
+import { useReducer } from "react";
 import { createContext } from "react";
+import { PersonModel } from "../api/person";
 
 export const PeopleContext = createContext(null);
 export const PeopleDispatchContext = createContext(null);
+
+interface childrenProps {
+  children: JSX.Element;
+}
+
+export function PeopleProvider({ children }: childrenProps) {
+  const [people, dispatch] = useReducer(peopleReducer, [])
+  return (
+    <PeopleContext.Provider value={people} >
+      <PeopleDispatchContext.Provider value={dispatch}>
+        {children}
+      </PeopleDispatchContext.Provider>
+    </PeopleContext.Provider>
+  )
+}
+
+function peopleReducer(people: PeopleState, action: PeopleAction) {
+  // TODO : Add, Remove, Update, Set Data
+  switch (action.type) {
+    default: {
+      return [...people];
+    }
+  }
+}
+
+enum PeopleActionKind {
+  ADD = 'ADD',
+  UPDATE = 'UPDATE',
+  REMOVE = 'REMOVE'
+}
+
+interface PeopleAction {
+  type: PeopleActionKind;
+  payload: PersonModel;
+}
+
+type PeopleState = [PersonModel]

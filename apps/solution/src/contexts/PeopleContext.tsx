a65b2879b15c58955e86dc10dc33e29ed3dc80c3
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-09-28 11:36:58 +0200

    feat(peopleContext): add and remove from context

--- apps/solution/src/contexts/PeopleContext.tsx
+++ apps/solution/src/contexts/PeopleContext.tsx
@@ -3,12 +3,12 @@ import { useContext } from "react";
 import { createContext } from "react";
 import { PersonModel } from "../api/person";
 
-interface PeopleContext {
+interface PeopleContextTypes {
   people: PersonModel[],
   dispatch: React.Dispatch<PeopleAction> | null
 }
 
-export const PeopleContext = createContext<PeopleContext>({ people: [], dispatch: null });
+export const PeopleContext = createContext<PeopleContextTypes>({ people: [], dispatch: null });
 
 interface childrenProps {
   children: JSX.Element;
@@ -24,26 +24,32 @@ export function PeopleProvider({ children }: childrenProps) {
 }
 
 type PeopleState = PersonModel[]
+enum PeopleActionKind {
+  ADD = 'ADD',
+  UPDATE = 'UPDATE',
+  REMOVE = 'REMOVE'
+}
+interface PeopleAction {
+  type: PeopleActionKind;
+  person: PersonModel;
+}
+
 function peopleReducer(people: PeopleState, action: PeopleAction) {
   // TODO : Add, Remove, Update, Set Data
   switch (action.type) {
+    case PeopleActionKind.ADD: {
+      return [...people, action.person]
+    }
+    case PeopleActionKind.REMOVE: {
+      return [...people.filter(person => person.id !== action.person.id)]
+    }
     default: {
       return [...people];
     }
   }
 }
 
-enum PeopleActionKind {
-  ADD = 'ADD',
-  UPDATE = 'UPDATE',
-  REMOVE = 'REMOVE'
-}
-
-interface PeopleAction {
-  type: PeopleActionKind;
-  person: PersonModel;
-}
-
+// Utils
 export function usePeople() {
   return useContext(PeopleContext).people;
 }

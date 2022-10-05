da99395046135905240dc200de9e26fa0a042c14
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-10-05 11:32:42 +0200

    test(util): added malformed and empty string date

--- libs/shared/util-date-fns/src/lib/shared-util-date-fns.spec.ts
+++ libs/shared/util-date-fns/src/lib/shared-util-date-fns.spec.ts
@@ -4,4 +4,12 @@ describe('sharedUtilDateFns', () => {
   it('should convert date', () => {
     expect(normalizeDate("2015-10-04T00:00:00.000Z")).toEqual('2015-10-04');
   });
+
+  it('should not accept malformed date', () => {
+    expect(normalizeDate("2015-13-04T00:00:00.000Z")).toBe('Invalid Date');
+  });
+
+  it('should not accept undefined', () => {
+    expect(normalizeDate('')).toBe('Invalid Date');
+  });
 });

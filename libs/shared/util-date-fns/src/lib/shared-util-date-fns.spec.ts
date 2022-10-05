03ad5c5d4fcc628c5f3618e586ec4f8b01541e0b
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-10-05 12:00:24 +0200

    test: used test.each for same test

--- libs/shared/util-date-fns/src/lib/shared-util-date-fns.spec.ts
+++ libs/shared/util-date-fns/src/lib/shared-util-date-fns.spec.ts
@@ -1,15 +1,11 @@
 import { normalizeDate } from './shared-util-date-fns';
 
 describe('sharedUtilDateFns', () => {
-  it('should convert date', () => {
-    expect(normalizeDate("2015-10-04T00:00:00.000Z")).toEqual('2015-10-04');
-  });
-
-  it('should not accept malformed date', () => {
-    expect(normalizeDate("2015-13-04T00:00:00.000Z")).toBe('Invalid Date');
-  });
-
-  it('should not accept undefined', () => {
-    expect(normalizeDate('')).toBe('Invalid Date');
-  });
+  test.each([
+    { date: "2015-10-04T00:00:00.000Z", expected: '2015-10-04' },
+    { date: "2015-13-04T00:00:00.000Z", expected: 'Invalid Date' },
+    { date: "", expected: 'Invalid Date' }
+  ])('normalize date correctly', ({ date, expected }) => {
+    expect(normalizeDate(date)).toBe(expected);
+  })
 });

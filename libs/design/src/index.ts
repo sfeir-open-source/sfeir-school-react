c1c4f4028a471c2158188c28abc1238ee07455af
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-10-05 18:19:00 +0200

    fix: exporting design libraries

--- libs/design/src/index.ts
+++ libs/design/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/panel/panel';
 export * from './lib/grid/grid';
 export * from './lib/iconized/iconized';
 export * from './lib/buttons';
+export * from './lib/title/title';
+export * from './lib/input/input';

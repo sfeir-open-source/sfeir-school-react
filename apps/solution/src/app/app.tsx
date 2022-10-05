a68263eba6fa812c6ce5a39ab60774d5650822b1
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-10-05 18:18:47 +0200

    feat(route): New route and header component

--- apps/solution/src/app/app.tsx
+++ apps/solution/src/app/app.tsx
@@ -3,27 +3,35 @@ import EditPeople from '../components/EditPerson/EditPerson';
 import styles from './app.module.scss';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { PeopleProvider } from '../contexts/PeopleContext';
+import NewPersonForm from '../components/AddPerson/new-person-form';
 
 export function App() {
   return (
     <div className={styles.app}>
       <BrowserRouter>
-        <header>
-          <Link to="/">
-            <h1>SF≡IR People</h1>
-          </Link>
-        </header>
+        <Header />
         <main>
           <PeopleProvider>
             <Routes>
               <Route path="/" element={<People />} />
               <Route path="/edit/:id" element={<EditPeople />} />
+              <Route path="/people/new" element={<NewPersonForm />} />
             </Routes>
           </PeopleProvider>
         </main>
       </BrowserRouter>
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">
+        <h1>SF≡IR People</h1>
+      </Link>
+    </header>
+  )
 }
 
 export default App;

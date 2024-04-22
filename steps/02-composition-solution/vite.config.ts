2251e2159b8f1c93bae9b30ff40861821797fe48
Anthony Pena <anthony.pena@outlook.fr>
2024-04-22 21:03:22 +0200

    feat: improve api path with proxy

--- steps/02-composition-solution/vite.config.ts
+++ steps/02-composition-solution/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': { target: 'http://localhost:3000', rewrite: (path) => path.substring('/api'.length) },
+      '/portraits': 'http://localhost:3000',
+    },
+  },
+});

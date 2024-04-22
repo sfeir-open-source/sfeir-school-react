0b2894a91750b01018e2abaacc4a7d83077af493
Anthony Pena <anthony.pena@outlook.fr>
2024-04-23 00:04:59 +0200

    feat: more repo tests

---
+++ tests/steps.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe.todo('every exercise or solution should be declared in the workspace');
+describe.todo('every exercise or solution should have a dedicated script with the same name');
+describe.todo("solution's README.md should be identical to exercise's one");
+describe.todo("exercise's package.json name should have same name as the directory");
+describe.todo("solution's package.json name should have same name as the directory");

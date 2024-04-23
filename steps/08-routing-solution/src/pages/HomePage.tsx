e9359fdd196596f52e196502e1cf4f93b3bc4265
Anthony Pena <anthony.pena@outlook.fr>
2024-04-23 14:04:16 +0200

    improve routing solution

--- steps/08-routing-solution/src/pages/HomePage.tsx
+++ steps/08-routing-solution/src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Carousel } from '../components/Carousel';
 import { PersonCard } from '../components/PersonCard';
 import { usePeople } from '../hooks/use-people';
-import { Layout } from './Layout';
 
 export function HomePage() {
   const people = usePeople();
@@ -10,12 +9,12 @@ export function HomePage() {
     return <></>;
   }
   return (
-    <Layout>
+    <>
       <h2>Let's meet!</h2>
       <PersonCard person={randomPerson} />
       <h2>All your colleagues!</h2>
       <Carousel people={people} />
-    </Layout>
+    </>
   );
 }
 
